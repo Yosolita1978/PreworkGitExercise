f2308495af8b12859b2911bb5f91eefd3605fe0a
Cristina Rodriguez <yosola@gmail.com>
2022-06-21 13:15:53 -0700

    first fuction working

---
+++ functions.js
@@ -0,0 +1,8 @@
+function hello(name){
+    return `Hello, ${name}, have a good day!`
+}
+
+console.log(hello("Yolis"));
+console.log(hello("Xochitl"));
+console.log(hello("Alice"));
+console.log(hello("Cristina"));
